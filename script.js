83914e2228db2558be96e33c0104bfab441725de
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-06-23 11:01:58 +0400

    refactored

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use-strict";
 
-//////////////////// Initial Variables ////////////////////
-
+//////////////////// Initial Variables and Parameters ////////////////////
 const gameBoxes = document.querySelectorAll(".grid__el");
 const btnNewGame = document.querySelector(".btn__newGame");
 const btnSaveName = document.querySelectorAll(".save__name");
@@ -26,6 +25,7 @@ const bodyBox = document.querySelector(".body__box");
 const title = document.querySelector(".game__name");
 const resetGame = document.querySelector(".btn__reset__yes");
 const noResetGame = document.querySelector(".btn__reset__no");
+const modalMessage = document.querySelector(".modal-message");
 let grid = [
   [" ", " ", " ", " ", " ", " "],
   [" ", " ", " ", " ", " ", " "],
@@ -49,6 +49,12 @@ let currentScoreP2 = 21;
 let totalScore1 = 0;
 let totalScore2 = 0;
 
+/**
+ * supporting function that hides/shows elements (вспомогательная функция для отображения/скрытия элементов)
+ * @param {DOM-element, string}
+ * @returns {undefined}
+ * @author Dmitriy Vnuchkov
+ */
 const showHideElement = function (el, to) {
   if (to === "show") {
     el.classList.remove("hidden");
@@ -64,7 +70,12 @@ const showHideElement = function (el, to) {
   }
 };
 
-//function to start a new game
+/**
+ * save the earned points and make variables return to default parameters to start a new round (сохраняет заработанные очки и возвращает изначальные значения переменных чтобы начать новый раунд)
+ * @param {}
+ * @returns {undefined}
+ * @author Dmitriy Vnuchkov
+ */
 const startNewGame = function () {
   if (result === "Yellow") {
     totalScoreP1.textContent = `Total score:${(totalScore1 += currentScoreP1)}`;
@@ -98,7 +109,12 @@ const startNewGame = function () {
   labelCurScoreP2.textContent = `Current Score: ${currentScoreP2}`;
 };
 
-///////////// Enter n Save Player's Names /////////////////
+/**
+ * these two functions save players' names (две функции ниже сохраняют имена игроков)
+ * @param {DOM-element}
+ * @returns {undefined}
+ * @author Dmitriy Vnuchkov
+ */
 btnSaveName.forEach((btn) => {
   btn.addEventListener("click", function () {
     if (btn.classList.contains("player1") && inputNameP1.value !== "") {
@@ -119,12 +135,16 @@ document.addEventListener("keyup", function (e) {
   }
 });
 
-///////////// One move by a player /////////////////
+/**
+ * processes the player's move (обрабатывает ход игрока)
+ * @param {event object}
+ * @returns {undefined}
+ * @author Dmitriy Vnuchkov
+ */
 gridArea.addEventListener("click", function (e) {
   e.preventDefault();
-  //Identify the column, where we shall add the color
+  // identify the column and the row, where the move has been done (определяет место, куда был сделан ход)
   const column = e.target.id;
-  //Find out, what box in the column can be filled with the color
   let i = 1;
   while (
     i < 7 &&
@@ -133,16 +153,19 @@ gridArea.addEventListener("click", function (e) {
   ) {
     i++;
   }
-  // If the column is full, ask to choose another column
+  // If the column is full, ask to choose another column (если колонка уже заполнена, показывает предупреждение игроку)
   if (i === 7) {
-    console.log("This column is full! Choose another column");
+    modalMessage.style.opacity = 1;
+    setTimeout(() => {
+      modalMessage.style.opacity = 0;
+    }, 2000);
   } else {
-    // Add the color to the UI
+    // Adds the color to the UI (добавленный цвет отображается для игрока)
     document.querySelector(
       `.box_${column}_${i}`
     ).style.backgroundColor = `${color}`;
     document.querySelector(`.box_${column}_${i}`).classList.add("shadowed");
-    // Moves record
+    // Moves record (сохраняет ход в UI)
     if (color === "Yellow") {
       player1moves += 1;
       movesP1.textContent = `Moves: ${player1moves}`;
@@ -154,12 +177,11 @@ gridArea.addEventListener("click", function (e) {
       currentScoreP2 -= 1;
       labelCurScoreP2.textContent = `Current Score: ${currentScoreP2}`;
     }
-
     movesP2.textContent = `Moves: ${player2moves}`;
-    // Add the color to JS Array
+    // adds the color to Array to check the result (добавляет ход в массив для проверки результата хода)
     move = `${column}_${color}`.split("_");
     whoIsWinner(move);
-    // Check the result and give turn to another player
+    // checks the result and give turn to another player (проверяет результат и передает ход другому игроку)
     if (result === "Draw") {
       color === "Yellow" ? (color = "Red") : (color = "Yellow");
       // Announce the result and hide the grid, if there is a winner
@@ -169,21 +191,17 @@ gridArea.addEventListener("click", function (e) {
           ? `${namePlayer1.textContent} is the winner!`
           : `${namePlayer2.textContent} is the winner!`
       }`;
-      console.log(`The Winner is ${result}`);
+      // announces the winner (Анонсирует победителя)
       showHideElement(blur, "show");
       showHideElement(endGameModal, "show");
     }
   }
 });
 
-//Start the new game / Cancel gameReset
+// adding event listeners (добавляет приемники событий)
 document.addEventListener("keyup", function (e) {
   if (e.key === "Escape" && !blur.classList.contains("hidden")) {
     startNewGame();
-  }
-  if (e.key === "Escape" && !resetGameModal.classList.contains("hidden")) {
-    // resetGameModal.classList.add("hidden");
-    showHideElement(resetGameModal, "hide");
   }
 });
 btnNewGame.addEventListener("click", function (e) {
@@ -191,25 +209,14 @@ btnNewGame.addEventListener("click", function (e) {
   startNewGame();
 });
 
-// //Reset the Game / Cancel gameReset
-// title.addEventListener("click", function () {
-//   // blur.classList.remove("hidden");
-//   // resetGameModal.classList.remove("hidden");
-//   showHideElement(blur, "show");
-//   showHideElement(resetGameModal, "show");
-// });
-// resetGame.addEventListener("click", function () {
-//   location.reload();
-// });
-
-// noResetGame.addEventListener("click", function () {
-//   // resetGameModal.classList.add("hidden");
-//   showHideElement(resetGameModal, "hide");
-//   // blur.classList.add("hidden");
-//   showHideElement(blur, "hide");
-// });
-/////////////////////// Game Mechanics //////////////////////
+/**
+ * checks the results of the move through the algorithm (проверяет ход игрока с помощью алгоритма)
+ * @param {array}
+ * @returns {string}
+ * @author Dmitriy Vnuchkov
+ */
 function whoIsWinner(move) {
+  // supporting function that etrieves move data and formats it for the algorithm (вспомогательная функция, которая обрабатыват и реформатирует информацию о ходе игрока)
   const moveMec = function (gr, move) {
     let j = 0;
     while (grid[gr][j] !== " ") {
@@ -219,6 +226,7 @@ function whoIsWinner(move) {
     return j;
   };
 
+  // four supporting functions for the algorithm (четыре вспомогательные функции для алгоритма)
   const checkHorizontal = (grid, x, a) => {
     let arr = [];
     for (let i = 0; i <= 6; i++) {
@@ -257,6 +265,7 @@ function whoIsWinner(move) {
     return arr.join("").includes(grid[x][a].repeat(4)) ? grid[x][a] : false;
   };
 
+  // supporting function that checks the move through all axes to determine if it was a winning move (вспомогательная функция которая проверяет ход по всем направлениям с помощью алгоритма, чтобы выяснить, был ли ход результативным)
   function moveCheck() {
     temp = checkHorizontal(grid, x, a);
     if (temp) winner = temp;
@@ -274,6 +283,7 @@ function whoIsWinner(move) {
   let winner;
   let x;
   let a = 0;
+  // triggers move check depending on the player's move (запускает проверку хода в зависимости от хода игрока)
   switch (move[0]) {
     case "A":
       x = 0;
@@ -318,39 +328,34 @@ function whoIsWinner(move) {
   } else return (result = "Draw");
 }
 
+/**
+ * saves the player's name (сохраняет имя игрока)
+ * @param {}
+ * @returns {undefined}
+ * @author Dmitriy Vnuchkov
+ */
 function saveName1() {
   namePlayer1.textContent = `${inputNameP1.value.toUpperCase()}`;
   inputNameP1.value = "";
   inputFieldP1.classList.add("hidden1");
   if (namePlayer2.textContent !== "Name") {
-    //  blur.classList.add("hidden");
     showHideElement(blur, "hide");
     document.querySelector(".instructions").classList.add("hidden");
   }
 }
 
+/**
+ * saves the player's name (сохраняет имя игрока)
+ * @param {}
+ * @returns {undefined}
+ * @author Dmitriy Vnuchkov
+ */
 function saveName2() {
   namePlayer2.textContent = `${inputNameP2.value.toUpperCase()}`;
   inputNameP2.value = "";
   inputFieldP2.classList.add("hidden1");
   if (namePlayer1.textContent !== "Name") {
-    // blur.classList.add("hidden");
     showHideElement(blur, "hide");
     document.querySelector(".instructions").classList.add("hidden");
   }
 }
-
-// CHANGING COLORS OF THE GRID SHADOW
-// function randomShadow() {
-//   const randomInt = (min, max) =>
-//     Math.floor(Math.random() * (max - min + 1) + min);
-
-//   setInterval(function () {
-//     const top = `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(
-//       0,
-//       255
-//     )})`;
-//     gridArea.style.boxShadow = `0px 0px 20px ${top}`;
-//   }, 750);
-// }
-// randomShadow();
